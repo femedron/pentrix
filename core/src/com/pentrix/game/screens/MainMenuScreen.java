4421737103e7a4364477f010dab379cabfdcb32b
AKTuBHbIu_roMeC <succ3917@gmail.com>
2024-06-17 16:15:19 +0300

    Main menu ui

--- core/src/com/pentrix/game/screens/MainMenuScreen.java
+++ core/src/com/pentrix/game/screens/MainMenuScreen.java
@@ -3,6 +3,7 @@ package com.pentrix.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
@@ -26,6 +27,7 @@ public class MainMenuScreen extends BaseScreen {
     Skin skin;
     Texture backgroundTexture = new Texture(Gdx.files.internal("mine/navalnyi.jpg"));
     Image bg;
+    BitmapFont font30, font50, minecraft50;
 
     public MainMenuScreen(Pentrix game) {
         super(game);
@@ -41,74 +43,94 @@ public class MainMenuScreen extends BaseScreen {
         stage.addActor(bg);
 
         skin = new Skin(Gdx.files.internal("skins/default_skin/uiskin.json"));
+
+        genFonts();
+    }
+
+    void genFonts(){
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/truetypefont/Amble-Bold.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 50;
+        font50 = generator.generateFont(parameter);
+        parameter.size = 30;
+        font30 = generator.generateFont(parameter);
+        generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/truetypefont/MinecraftBold.otf"));
+        parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 50;
+        minecraft50 = generator.generateFont(parameter);
+        generator.dispose(); // don't forget to dispose to avoid memory leaks!
     }
     @Override
     public void show() {
         init();
 
-        // Create a table that fills the screen. Everything else will go inside this table.
-        Table table = new Table();
-        table.setFillParent(true);
-        stage.addActor(table);
+        TextButton.TextButtonStyle buttonStyle = skin.get(TextButton.TextButtonStyle.class);
+        buttonStyle.font = font50;
+        buttonStyle.fontColor = Color.WHITE;
 
-        // Game name on top todo
-        Label gameNameLabel = new Label("Pentrix", skin);
-        table.add(gameNameLabel).colspan(2).expandX().padTop(20);
-        table.row();
+        Label.LabelStyle labelStyle = skin.get(Label.LabelStyle.class);
+        labelStyle.font = font30;
+        labelStyle.fontColor = Color.WHITE;
+
+        Label.LabelStyle bigLabel = new Label.LabelStyle(minecraft50, Color.WHITE);
+
+        Container<Label> container = new Container<Label>(new Label("Pentrix", bigLabel));
+        container.setFillParent(true);
+        container.center().top().padTop(50);
+        stage.addActor(container);
+        // Create a table that fills the screen. Everything else will go inside this table.
+        Table tableL = new Table();
+        Table tableR = new Table();
+        tableL.setDebug(true);
+        tableL.setFillParent(true);
+        tableL.center().left();
+        tableL.defaults().center().left().padLeft(20).spaceBottom(10);
+        stage.addActor(tableL);
+
+        tableR.setDebug(true);
+        tableR.setFillParent(true);
+        tableR.center().right();
+        tableR.defaults().center().right().padRight(20).spaceBottom(10);
+        stage.addActor(tableR);
 
         // Two lines of text in top right corner
         Label line1Label = new Label("Top score:", skin);
         Label line2Label = new Label("Last score:", skin);
-        table.add().expandX();
-        table.add(line1Label).right().padRight(20);
-        table.row();
-        table.add().expandX();
-        table.add(line2Label).right().padRight(20);
-        table.row();
-
-
+        tableL.add(line1Label);
+        tableL.row();
+        tableL.add(line2Label).spaceBottom(100);
+        tableL.row();
 
         //create buttons
         TextButton restart = new TextButton("Restart", skin);
+        Container restartContainer = new Container<>(restart);
         TextButton newGame = new TextButton("New Game", skin);
         TextButton leaderboard = new TextButton("Leaderboard", skin);
         TextButton settings = new TextButton("Settings", skin);
-        TextButton quit = new TextButton("Quit (TEMPORARY: END SCREEN (GAME OVER)", skin);
+        TextButton quit = new TextButton("Quit", skin);
 
+        tableL.add(restartContainer);
+        //restartContainer.setVisible(false);
+        tableL.row();
+        tableL.add(newGame);
+        tableL.row();
+        tableL.add(leaderboard);
+        tableL.row();
+        tableL.add(settings);
+        tableL.row();
+        tableL.add(quit);
+        tableL.row();
 
-        //todo: try to make buttons big, while preserving font sharpness (mb try using freetype fonts)
-        newGame.setTransform(true);
-        newGame.scaleBy(0.5f);
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/truetypefont/Amble-Bold.ttf"));
-        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        parameter.size = 24;
-        BitmapFont font24 = generator.generateFont(parameter);
-        generator.dispose(); // don't forget to dispose to avoid memory leaks!
-
-        //add buttons to table
-//        table.add(newGame).fillX().uniformX();
-//        table.row();//.pad(10, 0, 10, 0);
-//        table.add(leaderboard).fillX().uniformX();
-//        table.row();
-//        table.add(settings).fillX().uniformX();
-//        table.row();
-//        table.add(quit).fillX().uniformX();
-        table.add(restart).left().padLeft(20).padBottom(10);  // todo: this button could be hidden, keep in mind when positioning
-        table.row();
-        table.add(newGame).left().padLeft(20).padBottom(10);
-        table.row();
-        table.add(leaderboard).left().padLeft(20).padBottom(10);
-        table.row();
-        table.add(settings).left().padLeft(20).padBottom(10);
-        table.row();
-        table.add(quit).left().padLeft(20).padBottom(10);
-        table.row();
 
         // Text on the right (game tutorial)
         Label textLabel = new Label("This is some text on the right sideccccccccccccccccccccc" +
                 "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccc.", skin);
-        table.add().expandX();
-        table.add(textLabel).right().padRight(20);
+        textLabel.setWrap(true);
+        tableR.add(textLabel).prefWidth(WIDTH/3).maxHeight(HEIGHT*3/4);
+
+
+
+
 
         restart.addListener(new ChangeListener() {
             @Override
